ef8bece834fce70ac23f85f02120d204be0d919e
Johan Sternerup <johan.sternerup@gmail.com>
2025-12-04 06:52:48 +0100

    Start day 4

--- day04/day04.py
+++ day04/day04.py
@@ -1,65 +1,44 @@
 #!/usr/bin/env python3
 
-def get_banks(fname):
+def get_grid(fname):
     aa = []
     with open(fname, "r") as file:
         for line in file:
             l = line.strip()
-            a = [int(c) for c in l]
-            aa.append(a)
+            aa.append(l)
     return aa
 
-def get_max_jolt(a):
-    idcs = []
-    mx = -1
-    for i, n in enumerate(a[:-1]):
-        if n > mx:
-            idcs = [i]
-            mx = n
-        elif n == mx:
-            idcs.append(i)
-            mx = n
-    mx2 = -1
-    for n in a[idcs[0]+1:]:
-        if n > mx2:
-            mx2 = n
-    return mx * 10 + mx2
-
 def part1(fname):
-    aa = get_banks(fname)
-    acc = 0
-    for a in aa:
-        mx = get_max_jolt(a)
-        # print(mx)
-        acc = acc + mx
+    aa = get_grid(fname)
+    ly = len(aa)
+    lx = len(aa[0])
+    poss = set()
+    for y0 in range(ly):
+        for x0 in range(lx):
+            count = 0
+            cposs = set()
+            if aa[y0][x0] != '@':
+                continue
+            for dy in range(-1, 2):
+                for dx in range(-1, 2):
+                    x = x0 + dx
+                    y = y0 + dy
+                    if x < 0 or x >= lx or y < 0 or y >= ly:
+                        continue
+                    if aa[y][x] == '@':
+                        count = count + 1
+                        cposs.add((y, x))
+            if count < 4:
+                for p in cposs:
+                    poss.add(p)
+    acc = len(poss)
     print(f"Sum: {acc}")
-
-def get_max_jolt_12(a):
-    jolt = 0
-    bidx = 0
-    for ndig in range(12):
-        mx = -1
-        nidx = -1
-        for i, n in enumerate(a[bidx:len(a) - 11 + ndig]):
-            if n > mx:
-                mx = n
-                nidx = i + bidx + 1
-        jolt = jolt * 10 + mx
-        bidx = nidx
-        # print(mx, bidx, len(a) - ndig)
-    return jolt
 
 def part2(fname):
-    aa = get_banks(fname)
+    aa = get_grid(fname)
     acc = 0
-    # get_max_jolt_12(aa[0])
-    # return
-    for a in aa:
-        mx = get_max_jolt_12(a)
-        # print(mx)
-        acc = acc + mx
     print(f"Sum: {acc}")
 
 
-part1("input.txt")
-part2("input.txt")
+part1("ex.txt")
+# part2("input.txt")
