6baec293197b28f3ea5fd0e8622ce9563fd37777
Johan Sternerup <johast@axis.com>
2025-12-05 08:31:42 +0100

    Day 5 part 2 - How to succeed without deserving it
    Sometimes it's possible to go down the wrong path and still find a way
    out. I could have cleaned this up with a better algorithm, but let's
    keep it for public enjoyment.
    
    Change-Id: If74361daf98078e0c586fe8bd945fcb2d6b031cc

--- day05/day05.py
+++ day05/day05.py
@@ -30,34 +30,51 @@ def part1(fname):
             acc = acc + 1
     print(f"Sum: {acc}")
 
+def really_bad_algorithm(rs):
+    ro = []
+    for i in range(len(rs)):
+        # print(i, rs)
+        r = rs[i]
+        if r == (-1, -1):
+            continue
+        for j in range(i+1, len(rs)):
+            rr = rs[j]
+            if rr == (-1, -1):
+                continue
+            # print(r, rr)
+            if r[1] < rr[0] or r[0] > rr[1]:
+                # print("non-overlapping")
+                continue
+            if r[0] >= rr[0] and r[1] <= rr[1]:
+                # print("r is contained by rr, discard r")
+                r = (-1, -1)
+                break
+            elif r[0] <= rr[0] and r[0] >= rr[1]:
+                # print("r constains rr, discard rr")
+                rs[j] = (-1, -1)
+            else :
+                # print("overlapping, so extend r, discard rr")
+                r = ((min(r[0], rr[0]) , max(r[1], rr[1])))
+                rs[i] = r
+                rs[j] = (-1, -1)
+        if r != (-1, -1):
+            ro.append(r)
+    return ro
+
 def part2(fname):
     rs, _ = get_data(fname)
     acc = 0
-    ro = rs
-    for r in rs:
-        rn = []
-        for rr in ro:
-            if r[1] < rr[0]:
-                # r to the left of rr, keep both
-                rn.append(r)
-                rn.append(rr)
-            elif r[0] > rr[1]:
-                # r to the right of rr, keep both
-                rn.append(r)
-                rn.append(rr)
-            elif r[0] >= rr[0] and r[1] <= rr[1]:
-                # r is contained by rr, only keep rr
-                rn.append(rr)
-            elif r[0] <= rr[0] and r[0] >= rr[1]:
-                # r constains rr, only keep r
-                rn.append(r)
-            else:
-                # overlapping, so merge
-                rn.append((min(r[0], rr[0]), (max(r[1], rr[1])))
-        ro = rn
+    ro = []
+    while True:
+        # print(rs)
+        ro = really_bad_algorithm(rs)
+        # print(ro)
+        if rs == ro:
+            break
+        rs = ro
     for beg, end in ro:
         acc = acc + end - beg + 1
     print(f"Sum: {acc}")
 
 # part1("input.txt")
-part2("ex.txt")
+part2("input.txt")
