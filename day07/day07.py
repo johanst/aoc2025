ffee94ecd4df82a2af1d6623746bb23052dbf4b9
Johan Sternerup <johan.sternerup@gmail.com>
2025-12-07 08:37:50 +0100

    Day6 part 2

--- day07/day07.py
+++ day07/day07.py
@@ -29,8 +29,34 @@ def part1(fname):
 
 def part2(fname):
     a, s = get_data(fname)
-    acc = 0
+    b = {s: 1} # pos: number_of_paths for last row
+    for r in a[1:]:
+        bn = {}
+        beg = 0
+        while True:
+            idx = r.find('^', beg)
+            if idx == -1:
+                break
+            if idx in b:
+                pnum = b[idx]
+                del b[idx]
+                if idx - 1 in bn:
+                    bn[idx-1] = bn[idx-1] + pnum
+                else:
+                    bn[idx-1] = pnum
+                if idx + 1 in bn:
+                    bn[idx+1] = bn[idx+1] + pnum
+                else:
+                    bn[idx+1] = pnum
+            beg = idx + 1
+        for pos, pnum in b.items():
+            if pos in bn:
+                bn[pos] = bn[pos] + pnum
+            else:
+                bn[pos] = pnum
+        b = bn
+    acc = sum([v for v in b.values()])
     print(f"Sum: {acc}")
 
 part1("input.txt")
-# part2("input.txt")
+part2("input.txt")
