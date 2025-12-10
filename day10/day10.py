1536a4eb01b0c35a99e04c61e9b08fb8332865a7
Johan Sternerup <johast@axis.com>
2025-12-10 16:02:01 +0100

    save some prints
    Change-Id: I06dd6a1258f0ce0870add63b757ccb06b5553d44

--- day10/day10.py
+++ day10/day10.py
@@ -58,7 +58,58 @@ def get_num_clicks(aa):
             visited[ncand] = cost_cand
     return 0
 
-assert get_num_clicks(('.###.#', [(0,1,2,3,4), (0,3,4), (0,1,2,4,5), (1,2)], (10,11,11,5,10,5))) == 2
+# assert get_num_clicks(('.###.#', [(0,1,2,3,4), (0,3,4), (0,1,2,4,5), (1,2)], (10,11,11,5,10,5))) == 2
+
+def bdiff_min(a, b):
+    return max([abs(na-nb) for na, nb in zip(a,b)])
+
+def get_num_clicks2(aa):
+    print("zzz")
+    ntargets = (aa[2])
+    ninits = [0 for _ in range(len(aa[2]))]
+    dinit = bdiff_min(ntargets, ninits)
+    visited = {tuple(ninits): 0}
+    hq = [(dinit, 0, ninits)] # minimum time to finish, cost, joltage levels
+    while len(hq) > 0:
+        mf, cost, nactuals  = heapq.heappop(hq)
+        print(f"evaluating nactuals={nactuals}, cost={cost} min_finish={mf} ntargets={ntargets}")
+        if nactuals == ntargets:
+            print("wtf")
+            return cost
+        for btns in aa[1]:
+            # print(f"   btns={btns}")
+            ncands = nactuals.copy()
+            ok = True
+            for idx in btns:
+                # print("    ", idx)
+                ncands[idx] = ncands[idx] + 1
+                if ncands[idx] > ntargets[idx]:
+                    print(f"btns={btns} => idx={idx} too high")
+                    ok = False
+                    break
+            if not ok:
+                continue
+            nc_tuple = tuple(ncands)
+            cost_cand = cost + 1
+            min_finish = bdiff_min(ntargets, ncands)
+            # print("hubba: ", nc_tuple, ntargets, cost_cand, min_finish)
+            if nc_tuple in visited:
+                if visited[nc_tuple] <= cost_cand:
+                    continue
+            print(f"   press {btns} => ncands={ncands}, cost={cost_cand} min_finish={min_finish}")
+            heapq.heappush(hq, (min_finish, cost_cand, ncands.copy()))
+            visited[nc_tuple] = cost_cand
+    print("Sorry")
+    return 0
+
+# Configuring the first machine's counters requires a minimum of 10 button
+# presses. One way to do this is by pressing (3) once, (1,3) three times, (2,3)
+# three times, (0,2) once, and (0,1) twice.
+num_clicks = get_num_clicks2(('.##.', [(3,), (1,3), (2,), (2,3), (0,2), (0,1)], [3,4,5,7]))
+print(num_clicks)
+assert num_clicks == 10
+
+assert get_num_clicks2(('.###.#', [(0,1,2,3,4), (0,3,4), (0,1,2,4,5), (1,2)], [10,11,11,5,10,5])) == 11
 
 def part1(fname):
     a = get_data(fname)
@@ -69,8 +120,11 @@ def part1(fname):
 
 def part2(fname):
     a = get_data(fname)
+    # print(a)
     acc = 0
+    for aa in a:
+        acc = acc + get_num_clicks2(aa)
     print(f"Sum: {acc}")
 
-part1("input.txt")
-# part2("ex.txt")
+# part1("input.txt")
+part2("ex.txt")
