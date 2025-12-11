abb44bce4053609ade8756f6b462285cfb836e43
Johan Sternerup <johan.sternerup@gmail.com>
2025-12-11 07:29:18 +0100

    Day 11 - part 1

--- day11/day11.py
+++ day11/day11.py
@@ -2,18 +2,33 @@
 import heapq
 
 def get_data(fname):
-    a = []
+    a = {}
     with open(fname, "r") as file:
         for line in file:
-            ws = line.strip().split(',')
-            cs = (int(ws[0]), int(ws[1]))
-            a.append(cs)
-            assert(len(cs) == 2)
+            ws = line.strip().split()
+            assert ws[0][-1] == ':'
+            node = ws[0][:-1]
+            paths = ws[1:]
+            assert node not in a
+            a[node] = paths
     return a
+
+def num_paths_to_out(a, track):
+    if track[-1] == 'out':
+        return 1
+    acc = 0
+    for node in a[track[-1]]:
+        # print(track)
+        if node not in track:
+            track.append(node)
+            acc = acc + num_paths_to_out(a, track)
+            track.pop()
+    return acc
 
 def part1(fname):
     a = get_data(fname)
-    acc = 0
+    # print(a)
+    acc = num_paths_to_out(a, ['you'])
     print(f"acc: {acc}")
 
 def part2(fname):
@@ -22,5 +37,5 @@ def part2(fname):
     print(f"acc: {acc}")
 
 
-part1("ex.txt")
+part1("input.txt")
 # part2("ex.txt")
