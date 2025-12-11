4818277a39d6a5b712834f7e922644aeb878e077
Johan Sternerup <johan.sternerup@gmail.com>
2025-12-11 08:50:41 +0100

    Day11 - part 2

--- day11/day11.py
+++ day11/day11.py
@@ -31,11 +31,33 @@ def part1(fname):
     acc = num_paths_to_out(a, ['you'])
     print(f"acc: {acc}")
 
+def num_paths_to_out2(a, cache, from_node, to_node, have_dac, have_fft):
+    if to_node == 'out':
+        # print(track)
+        if have_dac and have_fft:
+            return 1
+        else:
+            return 0
+    have_dac_next = to_node == 'dac' or have_dac
+    have_fft_next = to_node == 'fft' or have_fft
+    acc = 0
+    for node_next in a[to_node]:
+        # print(track)
+        cache_key = (to_node, node_next, have_dac_next, have_fft_next)
+        if cache_key in cache:
+            acc = acc + cache[cache_key]
+        else:
+            acc = acc + num_paths_to_out2(a, cache, to_node, node_next, have_dac_next, have_fft_next)
+    cache[(from_node, to_node, have_dac, have_fft)] = acc
+    return acc
+
 def part2(fname):
     a = get_data(fname)
     acc = 0
+    cache = {}
+    for node_next in a['svr']:
+        acc = acc + num_paths_to_out2(a, cache, 'svr', node_next, False, False)
     print(f"acc: {acc}")
 
-
 part1("input.txt")
-# part2("ex.txt")
+part2("input.txt")
