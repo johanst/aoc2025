94dc6f4ab941d26fce50568d0009dde5855e0433
Johan Sternerup <johan.sternerup@gmail.com>
2025-12-13 15:53:13 +0100

    Day 12 done

--- day12/day12.py
+++ day12/day12.py
@@ -5,14 +5,27 @@ def get_data(fname):
     with open(fname, "r") as file:
         for line in file:
             l = line.strip()
-            # ws = line.strip().split(',')
-            # cs = (int(ws[0]), int(ws[1]), int(ws[2]))
-            a.append(l)
+            if 'x' in l:
+                lp = l.split(':')
+                wl = lp[0].split('x')
+                width, length = int(wl[0]), int(wl[1])
+                npack = [int(w) for w in lp[1].split()]
+                a.append((width, length, npack))
     return a
 
 def part1(fname):
     a = get_data(fname)
-    acc = 0
+    ps = []
+    for pdata in a:
+        width, height, packets = pdata
+        area = width * height
+        area_required = sum(packets) * 9
+        ps.append((area_required / area, area, area_required))
+
+    ps.sort()
+    for p in ps:
+        print(p)
+    acc = sum([1 for p in ps if p[0] < 1.1])
     print(f"Sum: {acc}")
 
 def part2(fname):
@@ -21,4 +34,4 @@ def part2(fname):
     print(f"Sum: {acc}")
 
 part1("input.txt")
-part2("input.txt")
+# part2("input.txt")
